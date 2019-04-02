30f3e28295c31f6bf4cfee64938475e166a507db
elletiburon <elleharrer@gmail.com>
2019-04-02 18:51:29 -0500

    initial files and styles

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+//html sections needed: header, game instructions, random number display, characters display, score display, win/loss display
+
+
+//define variables
+
+//generate random HP number 
+
+//display in correcrt div
+
+//add images to page. 
+
+//assign images random damage value
+
+//when player clicks on character button, a random number of points (damage) is added to their total HP
+
+//player WINS if total damage matches random HP number
+
+//player LOSES if total damage is above random HP number
+
+

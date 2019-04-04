855e0a733f7ec109a9e29d4fcd4e17461cfaeada
elletiburon <elleharrer@gmail.com>
2019-04-03 23:52:34 -0500

    click function random numbers and better pseudocode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,19 +2,55 @@
 
 
 //define variables
+var randomNumber =  Math.floor((Math.random() * 100) + 50);
+console.log(randomNumber)
+var counter = 0;
+var damageNumber = Math.floor(Math.random() * 21);
+console.log(damageNumber)
+var wins = 0;
+var losses = 0;
 
 //generate random HP number 
 
+
 //display in correcrt div
+$("#randomHP").text(randomNumber);
 
-//add images to page. 
+// TODO assign images random damage value with a function?
 
-//assign images random damage value
+$("#merle").attr("data-damagevalue", damageNumber);
 
-//when player clicks on character button, a random number of points (damage) is added to their total HP
+//when player clicks on character button, a random number of points damage is added to their total HP
 
+$(".character-image").on("click", function() {
+    var damageValue = ($(this).attr("data-damagevalue"));
+    damageValue = parseInt(damageValue);
+    counter += damageValue;
+    console.log(counter)
+    $("#damage").text(counter);
+
+//TODO figure out why the counter is not going up on immediate win/loss
 //player WINS if total damage matches random HP number
+if (counter === randomNumber) {
+    $("#wins").text(wins++);
+    $("#win-lose").text("YOU WIN!").css("color", "green")
+  }
 
 //player LOSES if total damage is above random HP number
+  else if (counter >= randomNumber) {
+    $("#losses").text(losses++);
+    $("#win-lose").text("YOU LOSE.").css("color", "red")   
+  }
+
+
+});
+
+//TODO reset function generate new randomNumber, assign new damageNumbers, clear win-lose div, reset counter to 0
+// function reset() {
+//     counter = 0;
+//     $("#win-lose").empty()
+//     $("#randomHP").text(randomNumber);
+     
+// }
 
 

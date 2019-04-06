8302299b037559e129202b3555a5808ff9af7f44
elletiburon <elleharrer@gmail.com>
2019-04-06 15:40:34 -0500

    reset function and random values work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,59 +2,87 @@
 
 
 //define variables
-var randomNumber =  Math.floor((Math.random() * 100) + 50);
+var randomNumber = 0;
 console.log(randomNumber)
 var counter = 0;
-var damageNumber = Math.floor(Math.random() * 21);
-console.log(damageNumber)
+// var damageNumber = Math.floor(Math.random() * 21);
+// console.log(damageNumber)
 var wins = 0;
 var losses = 0;
 
 
+$(document).ready(function(){
+    reset();
+});
 
 
-//generate random HP number 
 
-
-//display in correcrt div
-$("#randomHP").text(randomNumber);
-
-// TODO assign images random damage value with a function?
-
-$("#merle").attr("data-damagevalue", damageNumber);
 
 //when player clicks on character button, a random number of points damage is added to their total HP
 
-$(".character-image").on("click", function() {
+$(".character-image").on("click", function () {
+    $("#win-lose").text(" ");
+
     var damageValue = ($(this).attr("data-damagevalue"));
-    damageValue = parseInt(damageValue);
-    counter += damageValue;
+    damageValueIn = parseInt(damageValue);
+    counter += damageValueIn;
     console.log(counter)
     $("#damage").text(counter);
 
-//TODO figure out why the counter is not going up on immediate win/loss
-//player WINS if total damage matches random HP number
-if (counter === randomNumber) {
-    $("#wins").text(wins++);
-    $("#win-lose").text("YOU WIN!").css("color", "green")
-  }
+    
+    //player WINS if total damage matches random HP number
 
-//player LOSES if total damage is above random HP number
-  else if (counter >= randomNumber) {
-    $("#losses").text(losses++);
-    $("#win-lose").text("YOU LOSE.").css("color", "red")   
-    reset()
-  }
 
+    if (counter === randomNumber) {
+        $("#wins").text(++wins);
+        $("#win-lose").text("YOU WIN!").css("color", "green")
+        reset();
+        
+    }
+    //player LOSES if total damage is above random HP number
+    else if (counter > randomNumber) {
+        $("#losses").text(++losses);
+        $("#win-lose").text("YOU LOSE.").css("color", "red")
+        reset();
+        
+    }
+    console.log("test", counter, randomNumber, counter===randomNumber);
 
 });
 
-//TODO reset function generate new randomNumber, assign new damageNumbers, clear win-lose div, reset counter to 0
+
+
+//reset funtion clears and resets all the shit that needs clearing and resetting
+
 function reset() {
+
     counter = 0;
-    $("#win-lose").empty();
+    $(this).attr("data-damagevalue",'0');
+
+    randomNumber = Math.floor((Math.random() * 100) + 50);
     $("#randomHP").text(randomNumber);
-     
+
+    var merleR= Math.floor(Math.random() * 21) + 1;
+    $("#merle").attr("data-damagevalue",merleR);
+    var taakoR= Math.floor(Math.random() * 21) + 1;
+    if (merleR == taakoR){
+        taakoR= Math.floor(Math.random() * 21) + 1;
+    }
+    $("#taako").attr("data-damagevalue", taakoR);
+    var magnusR= Math.floor(Math.random() * 21) + 1;
+    if (magnusR == taakoR || magnusR == merleR){
+        magnusR= Math.floor(Math.random() * 21) + 1;
+    }
+    $("#magnus").attr("data-damagevalue", Math.floor(Math.random() * 21) + 1);
+    var angusR = Math.floor(Math.random() * 21) + 1;
+    if (angusR == taakoR || angusR == merleR || angusR == magnusR){
+        angusR= Math.floor(Math.random() * 21) + 1;
+    }
+    
+    $("#angus").attr("data-damagevalue", Math.floor(Math.random() * 21) + 1);
+
+   
+
 }
 
 //TODO loop audio FOR SURE maybe make each onclick have a boop sound if you have time
